b96671689b1872d85f662e2b57c55a7597e6433c
cathy <cathynoxo@gmail.com>
2017-01-10 00:54:30 +0800

    add user_id to group

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  belongs_to :user
-  validates :title, presence: true
-end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :groups
 end
